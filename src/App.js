abaab584b5ffb8f51d6fb370ba423dde16e1139c
Palaye <k8zunv@splm.siemens.com>
2021-04-05 15:05:05 +0530

    menu component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
+import Menu from './components/MenuComponent'
 
 function App() {
   return (
-    <div className="App">
+    <div className>
       <Navbar dark  color="success">
         <div className="container">
           <NavbarBrand href="/">Ristorante</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
